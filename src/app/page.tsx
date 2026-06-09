2b73a622ad99d3c6aef9b73e7020c36262fab48d
singlavikas123-boop <singla.vikas123@gmail.com>
2026-06-09 18:29:18 +0530

    Add home page

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Hero from '@/components/Hero'
+import Features from '@/components/Features'
+import CoursesPreview from '@/components/Courses'
+import Testimonials from '@/components/Testimonials'
+import CTA from '@/components/CTA'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <CoursesPreview />
+      <Testimonials />
+      <CTA />
+    </>
+  )
+}
